a3b2b9d5e90962631beda1064672410c7ea95e8e
Franco Silva <francosilva@Francos-MacBook-Pro.local>
2020-02-23 22:04:38 -0600

    new commit

--- src/components/Chirper.jsx
+++ src/components/Chirper.jsx
@@ -2,10 +2,22 @@ import React, { Component } from 'react'
 import TimeLine from './TimeLine';
 
 export class Chirper extends Component {
+
+    getStyle() {
+        return {
+            padding: '5px',
+            width: '500px'
+        }
+    }
+
     render() {
-        return this.props.array.map((chip) => (
-            <TimeLine chip2 = { chip } />
-        ))
+        return (
+            <div style = { this.getStyle() }>
+            {this.props.array.map((chip) => (
+            <TimeLine key = { chip.id } chip2 = { chip } />
+            ))}
+            </div>
+        )
     }
 }
 
